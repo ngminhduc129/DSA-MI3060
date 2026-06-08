152685fb07e027c61d0f83f539c9d51d35c4fd4c
ngockienngudot-ctrl <ngockienngudot@gmail.com>
2026-06-08 09:12:28 +0700

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import json
 import heapq
-from loadData import load_data
+from loaddata import load_data
 from MinHeap_Graph import Graph
 from findShortestPath_reconstructPath import findShortestPath
 from PIL import Image, ImageTk
